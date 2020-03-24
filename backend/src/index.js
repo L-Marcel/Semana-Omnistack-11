29c69b3052fa00bb8e27e6a01e7445b22e6ad0f2
UmbraeVespery <62476762+UmbraeVespery@users.noreply.github.com>
2020-03-24 15:20:56 -0300

    Back-end da aplicação

---
+++ backend/src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const routes = require('./routes');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+/**
+ * app.use(cors({
+ *  origin: 'http://meuapp.com
+ * }));
+ * 
+ */
+
+app.use(express.json());
+app.use(routes);
+
+
+
+app.listen(3333);
+
